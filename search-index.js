18706311f361498a376b84896a8ad468d3371394
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-09 17:26:23 +0000

    Deploy danylaporte/lock_derive to github.com/danylaporte/lock_derive.git:gh-pages

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+
+searchIndex["lock_derive"]={"doc":"A derive proc macro allowing to locks simultaneously…","i":[[14,"locks","lock_derive",E,N,N]],"p":[]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["lock_derive"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
